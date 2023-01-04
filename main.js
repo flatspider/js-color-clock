916175b08f831e3a625e304750dd2ba587ee331a
Conor McManamon <c.s.mcmanamon@gmail.com>
2023-01-04 13:50:31 -0500

    Outlined js requirements

---
+++ main.js
@@ -0,0 +1,40 @@
+
+
+/*
+
+
+
+console.log the current time on page load
+console.log the current time every second
+Display the current time every second
+Display the current time, padded with zeros to be exactly two digits long, every second
+console.log the percentage of a minute that the current seconds represents (e.g., if 30 seconds have elapsed, console.log 0.5)
+Using the percent above, dynamically modify the length of the timer bar
+console.log a hexidecimal color that is based on the current second, every second
+Dynamically update the background color of the clock using the hexidecimal color
+Display the generated color's hex value on hover
+Mission Accomplished!
+
+*/
+
+
+// Acquire the current time. 
+
+// Acquire the current time every second. 
+
+// Modify the clock text to be the actual time.
+
+// Pad the time return with zeros.
+
+// Determine what percentage of a minute has passed. Value / 60 = percentage
+
+// Set the length of the under timer bar to reflect the percentage. Width * percentage
+
+// Get a hexadecimal value for the gradient in the background. 
+
+// Update the hexadecimal each second to read out to the gradient. 
+
+// On hover over the clock, reveal the hexadecimal r g b value. 
+
+
+
