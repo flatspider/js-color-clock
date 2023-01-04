752e0a10bbc94b84bdf7ec3abf0d67a80d64ac82
Conor McManamon <c.s.mcmanamon@gmail.com>
2023-01-04 16:54:26 -0500

    Cleaned up formatting

--- README.md
+++ README.md
@@ -1,11 +1,3 @@
-This code creates a color changing clock.
-
-When you hover over the clock, the timer changes to hexadecimal.
-
-Have a color changing gradient in the back ground.
-
-As the seconds timer counts up, have a timeline that is under the clock that expands to the full length of the clock when the count is at 60.
-
 ----------CCS Notes:
 
 # Color Clock
--- main.js
+++ main.js
@@ -9,25 +9,22 @@ Display the current time, padded with zeros to be exactly two digits long, every
 console.log the percentage of a minute that the current seconds represents (e.g., if 30 seconds have elapsed, console.log 0.5)
 Using the percent above, dynamically modify the length of the timer bar
 console.log a hexidecimal color that is based on the current second, every second
+
 Dynamically update the background color of the clock using the hexidecimal color
+
 Display the generated color's hex value on hover
 Mission Accomplished!
 
 */
 
-var timer; // Need access to this variable outside of function call. Not working.
 
 var hoverTime = false;
 
 
 function timeUpdate(){
 
-
-    // Create a logic evaluation to determine what is displayed on the clock.
    
     var timer = new Date(); // Pulls the time from the computer.
-
-    
 
     // Get the normal time:
     var hours = timer.getHours().toString().padStart(2, "0"); // Turn num to string. Adds a zero to the front if return is not two digits.
@@ -39,13 +36,12 @@ function timeUpdate(){
     var hexMinutes = timer.getMinutes().toString(16).padStart(2, "0");
     var hexSeconds = timer.getSeconds().toString(16).padStart(2, "0");
 
-    console.log('#' + hexHours + ':' + hexMinutes + ':' + hexSeconds);
-    
 
-    // Updates clock display.
+    // Selects clock display.
     const clockView = document.querySelector(".clock-display");
 
 
+    // Sets the hoverTime variable based on the mouse position.
     clockView.addEventListener('mouseover', () => {
 
         
@@ -53,8 +49,6 @@ function timeUpdate(){
         
     
     });
-
-
     clockView.addEventListener('mouseout', () => {
 
         hoverTime = false;
@@ -63,7 +57,7 @@ function timeUpdate(){
 
 
 
-
+    // Checks if the clockView element is currently being hovered over and determines which numbers to display.
     if (hoverTime === false) {
         clockView.textContent = hours + ":" + minutes + ":" + seconds;
     } else {
@@ -71,7 +65,6 @@ function timeUpdate(){
     }
 
     
-
     // Creates growing and shrinking progress bar.
     var numSeconds = timer.getSeconds();
     let secondProgress = numSeconds / 60;
@@ -80,27 +73,16 @@ function timeUpdate(){
     let dynamicWidth = barWidth * secondProgress;
     clockBar.setAttribute('style', 'width:' + dynamicWidth + 'px');
     clockBar.clientWidth = clockBar.clientWidth * secondProgress;
-
-
-
-
-    var color1 = Math.floor(Math.random()*255);
-    //var hex1 = color1.toString(16);
-    //console.log(hex1);
-
-    // Change the background gradient every second that the clock updates.
-
+    
+    
+    // This sets the clock background color to three random RGB values.
     const clockBackground = document.querySelector(".clock");
-
+    var color1 = Math.floor(Math.random()*255); // Experimental color
     clockBackground.setAttribute('style', "background: rgb(" + color1 + ', ' + Math.random()*255 + ', ' + Math.random()*255 + ')' );
     
-    // HEX: clockBackground.setAttribute('style', "background: radial-gradient(#' + Math.random()*255 + ', ' + Math.random()*255 + ', ' + Math.random()*255 + ')' );
-    // background: radial-gradient(closest-side, #3f87a6, #ebf8e1, #f69d3c);
 
-
-
-    
-    setTimeout(timeUpdate, 250);
+    // Currently set to call the function once a second.
+    setTimeout(timeUpdate, 1000);
 
 }
 
@@ -109,45 +91,5 @@ timeUpdate(); // Initial function call.
 
 
 
-
-
-
-// Acquire the current time every second. 
-
-
-// Use the . class selector
-
-// Use querySelector to modify the clock.
-
-
-
-// Pad the time return with zeros.
-
-// Determine what percentage of a minute has passed. Value / 60 = percentage
-
-// Set the length of the under timer bar to reflect the percentage. Width * percentage
-
-
-
-
-
-
-
-
-// Get a hexadecimal value for the gradient in the background.
-
-// document query the background clock class
-
-// read out the hexadecimal value
-
-// update the hex value to change the background color
-
-// if when you hover over the clock, read out the hex value
-
-// Update the hexadecimal each second to read out to the gradient. 
-
-// On hover over the clock, reveal the hexadecimal r g b value. 
-
-const clockView = document.querySelector(".clock-display");
 
 
